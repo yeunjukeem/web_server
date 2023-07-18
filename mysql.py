947ce736be158b470d28be1240382dd3eb4132cf
Your Name <you@example.com>
2023-07-18 17:41:29 +0900

    Class Mysql: verify_password method-July182023

--- mysql.py
+++ mysql.py
@@ -1,6 +1,23 @@
 #mysql.py
 
 import pymysql
+# 암호화 알고리즘. 256을 제일 많이 사용한다.
+
+from passlib.hash import pbkdf2_sha256 
+
+
+# 원문 비밀번호를, 암호화 하는 함수
+
+def hash_password(original_password):
+    salt = '1234'
+    password = original_password + salt
+    password = pbkdf2_sha256.hash(password)
+    return password
+def check_password(input_password, hashed_password):
+    salt='eungok'
+    password = input_password + salt
+    result = pbkdf2_sha256.verify(password, hashed_password)
+    return result
 
 class Mysql:
     def __init__(self, host='localhost', user='root', db='os', password='', charset='utf8'):
@@ -11,7 +28,7 @@ class Mysql:
         self.charset = charset
 
     def get_user(self):
-        ret = []
+       
         db = pymysql.connect(host=self.host, user=self.user, db=self.db, password=self.password, charset=self.charset)
         curs = db.cursor()
         
@@ -28,16 +45,39 @@ class Mysql:
         curs = db.cursor()
         
         sql = '''insert into user (username, email, phone, password) values(%s,%s,%s,%s)'''
-        result = curs.execute(sql,(username, email, phone, password))
+        hashed_password = hash_password(password)
+        result = curs.execute(sql,(username, email, phone, hashed_password))
         print(result)
         db.commit()
         db.close()
+
+    def verify_password(self, password, email):    
+        db = pymysql.connect(host=self.host, user=self.user, db=self.db, password=self.password, charset=self.charset)
+        curs = db.cursor()
+
+        sql = f'SELECT * FROM user WHERE email = %s;'
+        curs.execute(sql, email)
+        
+        rows = curs.fetchall()             
+        print(rows)
+        # db.commit()
+        db.close()
+
+        if len(rows) != 0:
+            hashed_password = rows[0][4]
+            result = check_password(password, hashed_password)
+            if result:
+                print("SUCCESS")
+            else:
+                print("MissMatch Password!")
+        else:
+            print("User is not exist")
 
     def del_user(self, email):
         db = pymysql.connect(host=self.host, user=self.user, db=self.db, password=self.password, charset=self.charset)
         curs = db.cursor()
         
-        sql = "delete from user where email=%s"
+        sql =f"delete from user where email= %s;"
         curs.execute(sql,email)
         db.commit()
         db.close()
@@ -48,9 +88,13 @@ mysql = Mysql(password='java')
 
 # mysql.insert_user("garykim", "1@naver.com", "010-8496-9889", "1234")
 
-mysql.del_user("1@naver.com")
+# mysql.del_user("1@naver.com")
+# password = hash_pass
 
+# result = check_password("2234","$pbkdf2-sha256$29000$Paf0PickxHiv1bp3jhEiRA$zZskLaHOk5pvGdwyeAYKEx4oNyzmrw3UHPDuwprUZhY")
+# print(result)
 
+# mysql.verify_password("1234","2@naver.com")
 
-
+mysql.verify_password("1234", "1@naver.com" )
     
