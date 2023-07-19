32b9ba3469fba6b1f6e94cc717b9ba80a393500a
Your Name <you@example.com>
2023-07-19 17:36:31 +0900

    Day8 edit delete_July19

---
+++ app copy.py
@@ -0,0 +1,116 @@
+#app.py
+from flask import Flask, render_template, request, redirect, session
+from data import Articles
+from mysql import Mysql
+import config
+import pymysql
+# print(Articles())
+
+app = Flask(__name__)
+mysql = Mysql(password=config.PASSWORD)
+
+app.secret_key = "My_Key" #세션을 위해서 추가
+# session['is_logged_in'] =False
+# session['username'] = ''
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+#     if request.method == "GET":
+#         os_info = dict(request.headers)
+#         # req_data = request.get_json()
+#         print(os_info)
+#         name = request.args.get("name")
+#         print(name)
+#         hello = request.args.get("hello")
+#         print(hello)
+#         return render_template('index.html', header=f'{name}님, {hello} <3')
+#     elif request.method == "POST":
+#         data = request.form.get("name")
+#         data2 = request.form["hello"]
+#         print(data2)
+#         return render_template('index.html', header="안녕하세요.")
+
+    return render_template('index.html')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method =="GET":
+        return render_template('hello.html')
+    elif request.method == "POST":
+        name = request.form['name']
+        hello = request.form['hello']
+        return render_template('index.html', name=name, hello=hello)
+
+@app.route('/list', methods=['GET'])
+def list():
+    data = Articles()
+    return render_template('list.html', data=data)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        phone = request.form['phone']
+        password = request.form['password']
+        print(username, email, phone, password)
+        
+        
+        db = pymysql.connect(host=mysql.host, user=mysql.user, db=mysql.db, password=mysql.password, charset=mysql.charset)
+        curs = db.cursor()
+
+        sql = f'SELECT * FROM user WHERE email = %s;'
+        curs.execute(sql, email)
+        
+        rows = curs.fetchall()             
+        print(rows)
+        # 아래 오류방지를 위해서...
+        if rows:
+            return render_template('register.html', data = 1)
+        else:
+            result = mysql.insert_user(username, email, phone, password)
+            print(result)
+            return redirect('/login')
+    elif request.method == 'GET':
+        return render_template('register.html', data = 0)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method =='GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    
+        db = pymysql.connect(host=mysql.host, user=mysql.user, db=mysql.db, password=mysql.password, charset=mysql.charset)
+        curs = db.cursor()
+
+        sql = f'SELECT * FROM user WHERE email = %s;'
+        curs.execute(sql, email)
+        
+        rows = curs.fetchall()             
+        print(rows)
+        # 아래 오류방지를 위해서...
+        if rows:
+            result = mysql.verify_password(password, rows[0][4])
+            print(result)
+            if result:
+                session['is_logged_in'] = True
+                session['username'] = rows[0][1]
+                return redirect('/')
+                # return render_template('index.html', is_logged_in = session['is_logged_in'], username=session['username'])
+            else:
+                return redirect('/login')
+            return str(rows[0][4])
+        else:
+            return render_template('login.html')
+        
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- app.py
+++ app.py
@@ -1,105 +1,135 @@
-#app.py
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask , render_template ,request ,redirect ,session
 from data import Articles
 from mysql import Mysql
 import config
 import pymysql
+from datetime import timedelta
+
+
 # print(Articles())
+from functools import wraps
 
 app = Flask(__name__)
-app.secret_key = "My_Key" #세션을 위해서 추가
+app.secret_key = "eungok"
+# app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=1)
+
+
 mysql = Mysql(password=config.PASSWORD)
 
-@app.route('/', methods=['GET','POST'])
+def is_loged_in(func):
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        if 'is_loged_in' in session:
+            return func(*args, **kwargs)
+        else:
+            return redirect('/login')
+    return wrap
+
+@app.route('/' , methods=['GET','POST'])
+@is_loged_in #함수에 decorator('@')를 붙여주면, 
 def index():
-#     if request.method == "GET":
-#         os_info = dict(request.headers)
-#         # req_data = request.get_json()
-#         print(os_info)
-#         name = request.args.get("name")
-#         print(name)
-#         hello = request.args.get("hello")
-#         print(hello)
-#         return render_template('index.html', header=f'{name}님, {hello} <3')
-#     elif request.method == "POST":
-#         data = request.form.get("name")
-#         data2 = request.form["hello"]
-#         print(data2)
-#         return render_template('index.html', header="안녕하세요.")
+    # if request.method == "GET":
+    #     os_info = dict(request.headers)
+    #     print(os_info) 
+    #     name = request.args.get("name")
+    #     print(name)
+    #     hello = request.args.get("hello")
+    #     print(hello)
+    #     return render_template('index.html',header=f'{name}님 {hello}!!' )
+    
+    # elif request.method == "POST":
+    #     data  = request.form.get("name")
+    #     data_2 = request.form['hello']
+    #     print(data_2)
+        # return render_template('index.html',header= "안녕하세요 김태경입니다.!!!")
+    # print(session['is_loged_in'])
     return render_template('index.html')
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
     if request.method =="GET":
         return render_template('hello.html')
-    elif request.method == "POST":
+    
+    elif request.method =="POST":
         name = request.form['name']
         hello = request.form['hello']
-        return render_template('index.html', name=name, hello=hello)
+        return render_template('index.html', name=name , hello = hello)
 
-@app.route('/list', methods=['GET'])
+@app.route('/list', methods=['GET' , 'POST'])
 def list():
     data = Articles()
-    return render_template('list.html', data=data)
-
+    return render_template('list.html' , data=data)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        phone = request.form['phone']
-        password = request.form['password']
-        print(username, email, phone, password)
-        
+        username = request.form.get('username')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        password = request.form.get('password')
+        print(username , email , phone , password)
         
         db = pymysql.connect(host=mysql.host, user=mysql.user, db=mysql.db, password=mysql.password, charset=mysql.charset)
         curs = db.cursor()
 
         sql = f'SELECT * FROM user WHERE email = %s;'
-        curs.execute(sql, email)
+        curs.execute(sql , email)
         
-        rows = curs.fetchall()             
+        rows = curs.fetchall()
         print(rows)
-        # 아래 오류방지를 위해서...
         if rows:
-            return render_template('register.html', data = 1)
+
+            return render_template('register.html' , data = 1)
         else:
-            result = mysql.insert_user(username, email, phone, password)
+            result = mysql.insert_user(username, email ,phone,password )
             print(result)
             return redirect('/login')
-    elif request.method == 'GET':
-        return render_template('register.html', data = 0)
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    if request.method =='GET':
-        return render_template('login.html')
-    elif request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
     
+    elif request.method == "GET":
+        return render_template('register.html' , data= 0)
+    
+@app.route('/login',  methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
         db = pymysql.connect(host=mysql.host, user=mysql.user, db=mysql.db, password=mysql.password, charset=mysql.charset)
         curs = db.cursor()
 
         sql = f'SELECT * FROM user WHERE email = %s;'
-        curs.execute(sql, email)
+        curs.execute(sql , email)
         
-        rows = curs.fetchall()             
+        rows = curs.fetchall()
         print(rows)
-        # 아래 오류방지를 위해서...
+
         if rows:
             result = mysql.verify_password(password, rows[0][4])
-            print(result)
             if result:
-                session['is_logged_in'] = True
+                session['is_loged_in'] = True
                 session['username'] = rows[0][1]
+                print(session['is_loged_in'])
                 return redirect('/')
+                # return render_template('index.html', is_loged_in = session['is_loged_in'] , username=session['username'] )
             else:
                 return redirect('/login')
-            return str(rows[0][4])
         else:
             return render_template('login.html')
+        
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+@app.route('/edit/<ids>')
+def edit(ids):
+    return ids
+
+@app.route('/delete/<ids>')
+def delete(ids):
+    return ids
 
 if __name__ == '__main__':
+    app.config['SECRET_KEY'] = 'eungok'
     app.run(debug=True)
